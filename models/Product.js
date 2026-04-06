981a0e3cd88005fa2d16d310ada106894c6d6f6d
Alkendi-1 <a.alkendi26@gmail.com>
2026-04-06 10:57:10 +0200

    Create Product model

---
+++ models/Product.js
@@ -0,0 +1,20 @@
+// models/Product.js
+
+const db = require('../config/db');
+
+const Product = {
+  getAll: async () => {
+    const [rows] = await db.query('SELECT * FROM products');
+    return rows;
+  },
+
+  getById: async (id) => {
+    const [rows] = await db.query(
+      'SELECT * FROM products WHERE id = ?',
+      [id]
+    );
+    return rows[0];
+  }
+};
+
+module.exports = Product;
